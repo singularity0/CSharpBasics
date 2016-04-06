55bd02acdc468822a4ec0dc3917e7d759cabe67b
singularity0 <mr.mihail.petrov@gmail.com>
2016-04-06 17:34:24 +0300

    operators frist few tasks

---
+++ OperatorsExpressions/OddEven.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOrEven
+{
+    class OddEven
+    {
+        static void Main(string[] args)
+        {
+            int input_number = int.Parse(Console.ReadLine());
+            if (input_number % 2 == 0)    // ((input_number & 1) == 0)
+            {
+                Console.WriteLine("even " + input_number);
+            }
+            else
+            {
+                Console.WriteLine("odd " + input_number);
+
+            }
+
+
+        }
+    }
+}
+
+
+
